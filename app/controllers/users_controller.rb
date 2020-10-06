9628d7625b1521da4fe7eb61291a51e816597677
keisuke-sakuma <sakusuke0412@gmail.com>
2020-10-06 17:43:43 +0900

    追加:ストロングパラメータとusers#createアクションの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    user =User.new(user_params)
+
+    if user.save
+      redirect_to @user
+    else
+    end
   end
 
   def update
@@ -19,5 +25,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
   end
 end
