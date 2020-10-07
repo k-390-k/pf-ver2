30200d0aeb5bf224e221f407a0bf4d67f32f6ca7
keisuke-sakuma <sakusuke0412@gmail.com>
2020-10-07 11:42:10 +0900

    修正:カラム名を間違えていたので修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,6 +10,19 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    task = Task.new(task_params)
+    if task.save
+      redirect_to tasks_path
+    end
+  end
+
+
   def edit
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name,:description)
   end
 end
