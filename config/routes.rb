f4e6deb181fe5229527aeb4b0ed1d8cabe2226cd
keisuke-sakuma <sakusuke0412@gmail.com>
2020-10-07 11:24:15 +0900

    修正:tasksモデルのルーティングをresourcesへ

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/new'
-  get 'tasks/edit'
-  get 'index/show'
-  get 'index/new'
-  get 'index/edit'
+
   get '/login', to: 'sessions#new'
   post '/login', to:'sessions#create'
  resources :users
+ resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
